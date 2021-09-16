0a7f239fcc2d3b389cddf8dfea2163d3cff01fe2
Aleksandr802 <savs802@gmail.com>
2021-09-16 15:05:21 +0300

    add SearchBar

--- PhotoViewer/PhotoViewController.swift
+++ PhotoViewer/PhotoViewController.swift
@@ -13,15 +13,16 @@ class PhotoViewController: UIViewController {
     let basicQueryUrl = "https://picsum.photos/v2/list?"
     
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var searchBar: UISearchBar!
     @IBAction func unwindToMain(segue: UIStoryboardSegue){
         
     }
     
     private var photos: [Photo] = []
-    var page = 0
+    private var searchPhotos: [Photo] = []
+    var page = 1
     var fetchingMore = false
     var activityIndiator : UIActivityIndicatorView?
+    var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,44 +41,66 @@ class PhotoViewController: UIViewController {
         self.view.addSubview(activityIndiator!)
         self.view.bringSubviewToFront(activityIndiator!)
         
-        self.fetchData(page: 0)
+        self.fetchData(page: 1)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetY = scrollView.contentOffset.y
         let contentHeight = scrollView.contentSize.height
         
-        if (offsetY > contentHeight - scrollView.frame.height) && (!fetchingMore && page < 90) {
+        if (offsetY > contentHeight - scrollView.frame.height) && (!fetchingMore && page < 90) && !self.isSearching {
             activityIndiator?.startAnimating()
             self.view.bringSubviewToFront(activityIndiator!)
             page += 1
             print(page)
             fetchData(page: page)
-        }  else if (offsetY < 0.0) && (!fetchingMore && page > 1) {
+        } else if (offsetY < 0.0) && (!fetchingMore && page > 1) && !self.isSearching {
             activityIndiator?.startAnimating()
             self.view.bringSubviewToFront(activityIndiator!)
             page -= 1
             print(page)
             fetchData(page: page)
+        } else {
+            return
         }
+        
+        self.collectionView?.reloadData()
     }
     
     private func fetchData(page: Int, limit: Int = 30) {
         fetchingMore = true
-        print("Begin FetchData")
         let params = "page=" + "\(page)" + "&limit=" + "\(limit)"
         AF.request(basicQueryUrl + params, method: .get).responseDecodable(of: [Photo].self) { [weak self] response in
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
                 self?.photos = response.value ?? []
+                self?.searchPhotos = self?.photos ?? []
                 self?.fetchingMore = false
                 self?.collectionView.reloadData()
             }
         }
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.photos.removeAll()
+        self.isSearching = true
+        for item in self.searchPhotos {
+            if item.author.lowercased().contains(searchBar.text!.lowercased()) {
+                self.photos.append(item)
+            } else if item.id.contains(searchBar.text!) {
+                self.photos.append(item)
+            }
+        }
+        
+        if searchBar.text!.isEmpty {
+            self.photos = self.searchPhotos
+            self.isSearching = false
+        }
+        self.collectionView?.reloadData()
+    }
 
 }
 
-extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -89,13 +112,13 @@ extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSou
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotosCell", for: indexPath) as! PhotosCell
-        cell.photo = photos[indexPath.row]
+        cell.photo = photos[indexPath.item]
         activityIndiator?.stopAnimating()
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let photo = photos[indexPath.row]
+        let photo = photos[indexPath.item]
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let vc = storyboard.instantiateViewController(withIdentifier: "OnePhotoViewController") as? OnePhotoViewController {
             vc.photo = photo
@@ -103,5 +126,12 @@ extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSou
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionHeadersPinToVisibleBounds = true
+        
+        let searchView: UICollectionReusableView =  collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SearchBar", for: indexPath)
+        return searchView
+    }
+        
 }
-
---
+++ PhotoViewer/View/SearchBarView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchView.swift
+//  PhotoViewer
+//
+//  Created by Aleksandr Seminov on 16.09.2021.
+//
+
+import UIKit
+
+class SearchBarView: UICollectionReusableView {
+        
+    @IBOutlet weak var searchBar: UISearchBar!
+}
